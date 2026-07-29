262a90e9ec4ae49d03bdb9f017ebdb3b0c32b6da
Hugo Stelzeneder <hugostelzenedercardoso@gmail.com>
2026-07-29 13:11:52 -0300

    feat: adiciona rotas /sobre e /funcionarios/:id com req.params

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,18 @@ import "dotenv/config";
 import express from "express";
 
 const app = express();
+
 app.get("/", (req, res) => {
     res.send("Hello World! Servidor Express rodando.");
+});
+
+app.get("/sobre", (req, res) => {
+    res.send("Sistema de RH - API v1.0");
+});
+
+app.get("/funcionarios/:id", (req, res) => {
+    const id = req.params.id;
+    res.send(`Você pediu o funcionário com ID: ${id}`);
 });
 
 const PORT = process.env.PORT ?? "3000";
